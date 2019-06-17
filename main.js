b7f41164a963ce7e3365b291d45b5f355e8d9715
matthewcodepl <matthewcodepl@DESKTOP-EU96CO9.localdomain>
2019-06-17 21:37:41 +0200

    basic plane for the game

---
+++ main.js
@@ -0,0 +1,14 @@
+const el_1 = document.querySelector("#one");
+const el_2 = document.querySelector("#two");
+const el_3 = document.querySelector("#three");
+const el_4 = document.querySelector("#four");
+const el_5 = document.querySelector("#five");
+const el_6 = document.querySelector("#six");
+const el_7 = document.querySelector("#seven");
+const el_8 = document.querySelector("#eight");
+const el_9 = document.querySelector("#nine");
+const circle = document.querySelector(".circle");
+const exit = document.querySelector("exit");
+el_1.addEventListener("click", function test() {
+    console.log("work")
+})
